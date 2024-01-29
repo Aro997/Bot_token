bac38ea5275565027291650f2b570acd70f88d42
Aro997 <157995247+Aro997@users.noreply.github.com>
2024-01-29 20:28:32 +0300

    Update mody.py

--- mody.py
+++ mody.py
@@ -6,14 +6,13 @@ class Mody(object):
     
     OWNER = os.environ.get("OWNER", "6581896306")
     
-    ID_BOT = int(os.environ.get("ID_BOT"))
+    ID_BOT = int(os.environ.get("ID_BOT", ""))
     
-    USER_BOT = os.environ.get("USER_BOT")
+    USER_BOT = os.environ.get("USER_BOT", "")
     
-    MAX_ACCOUNTS = int(os.environ.get("MAX_ACCOUNTS"))
+    MAX_ACCOUNTS = int(os.environ.get("MAX_ACCOUNTS", ""))
     
     API_ID = os.environ.get("API_ID", "25281175")
     
     API_HASH = os.environ.get("API_HASH", "6d99cb2b60a2c519fc1f99bd19565730")
     
-
